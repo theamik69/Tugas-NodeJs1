d6d38807340da3630cc686a82a0e7a7832593936
theamik69 <amishbah12@gmail.com>
2023-05-09 21:57:16 +0700

    first commit

---
+++ moduleBiodata.js
@@ -0,0 +1,9 @@
+function biodata (nama, tempatLahir, tanggalLahir, alamat){
+    console.log(`Nama: ${nama}`);
+    console.log(`Tempat Lahir: ${tempatLahir}`);
+    console.log(`Tanggal Lahir: ${tanggalLahir}`);
+    console.log(`Alamat: ${alamat}`);
+}
+
+biodata("Ahmad Mishbah K", "Bandung", "12 Juli 1990", "Jl. Dago Pojok No.69/161B");
+
